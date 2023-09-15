cb3b71b90498c3b85024ea59bf1561dbaeef46cf
Tekk0n <bru.novaruiz@gmail.com>
2023-09-15 22:25:40 +0200

    Monitor and Visitor classes divided. High-Level Main.

---
+++ console-hw-monitor/MyMonitor.cs
@@ -0,0 +1,46 @@
+﻿using LibreHardwareMonitor.Hardware;
+
+namespace ConsoleHwMonitor
+{
+    public class MyMonitor
+    {
+        public void Monitor()
+        {
+            Computer computer = new Computer
+            {
+                IsCpuEnabled = true,
+                IsGpuEnabled = true,
+                IsMemoryEnabled = true,
+                IsMotherboardEnabled = true,
+                IsControllerEnabled = true,
+                IsNetworkEnabled = true,
+                IsStorageEnabled = true
+            };
+
+            computer.Open();
+            computer.Accept(new UpdateVisitor());
+
+            foreach (IHardware hardware in computer.Hardware)
+            {
+                Console.WriteLine("Hardware: {0}", hardware.Name);
+
+                foreach (IHardware subhardware in hardware.SubHardware)
+                {
+                    Console.WriteLine("\tSubhardware: {0}", subhardware.Name);
+
+                    foreach (ISensor sensor in subhardware.Sensors)
+                    {
+                        Console.WriteLine("\t\tSensor: {0}, value: {1}", sensor.Name, sensor.Value);
+                    }
+                }
+
+                foreach (ISensor sensor in hardware.Sensors)
+                {
+                    Console.WriteLine("\tSensor: {0}, value: {1}", sensor.Name, sensor.Value);
+                }
+            }
+
+            computer.Close();
+        }
+    }
+}
