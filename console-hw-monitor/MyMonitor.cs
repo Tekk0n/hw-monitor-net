336679f7443ad8385b716a1d71d14a2ac13d6034
Tekk0n <bru.novaruiz@gmail.com>
2023-09-21 19:38:26 +0200

    Only visits CPU,GPU and SensorType.Temperature

--- console-hw-monitor/MyMonitor.cs
+++ console-hw-monitor/MyMonitor.cs
@@ -10,21 +10,24 @@ namespace ConsoleHwMonitor
             {
                 IsCpuEnabled = true,
                 IsGpuEnabled = true,
-                IsMemoryEnabled = true,
-                IsMotherboardEnabled = true,
-                IsControllerEnabled = true,
-                IsNetworkEnabled = true,
-                IsStorageEnabled = true
+                //IsMemoryEnabled = true,
+                //IsMotherboardEnabled = true,
+                //IsControllerEnabled = true,
+                //IsNetworkEnabled = true,
+                //IsStorageEnabled = true
             };
 
             computer.Open();
             computer.Accept(new UpdateVisitor());
 
-            foreach (IHardware hardware in computer.Hardware)
+            //Iterates over desired hardware instead of all computer.Hardware
+            var filteredHardware = computer.Hardware.Where(hardware => hardware.HardwareType == HardwareType.Cpu || hardware.HardwareType == HardwareType.GpuNvidia);
+
+            foreach (IHardware hardware in filteredHardware)
             {
                 Console.WriteLine("Hardware: {0}", hardware.Name);
 
-                foreach (ISensor sensor in hardware.Sensors)
+                foreach (ISensor sensor in hardware.Sensors.Where(sensor => sensor.SensorType == SensorType.Temperature))
                 {
                     Console.WriteLine("\tSensor: {0}, value: {1}", sensor.Name, sensor.Value);
                 }
